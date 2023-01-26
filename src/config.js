d4174d49b7b3d22f85cee2ad6355dde1b8680cab
Sanjayvinoth7 <sanjayvinoth7@gmail.com>
2023-01-26 20:43:38 +0530

    w

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API = "https://twitter-backend-qmt0.onrender.com";
+export const API = "https://twitter-backend-qmt0.onrender.com/api";
 
 // export const API ="http://localhost:8000/api"
