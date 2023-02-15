29116e1647a5bf1dd3ea95aac2f3afe4d39cb7d1
Sanjayvinoth7 <sanjayvinoth7@gmail.com>
2023-02-15 14:00:45 +0530

    w

--- src/Components/RightSidebar/RightSidebar.js
+++ src/Components/RightSidebar/RightSidebar.js
@@ -4,10 +4,10 @@ const RightSidebar = () => {
   return (
     <div className="p-6 bg-slate-100 rounded-lg mx-4 space-y-4">
         <h2 className="font-medium">Trending</h2>
-        <p className="font-bold">#Thunivu</p>
-        <p className="font-bold">#Varisu</p>
-        <p className="font-bold">#Ak-62</p>
-        <p className="font-bold">#T-67</p>
+        <p className="font-bold">#Turkey</p>
+        <p className="font-bold">#ViratKholi</p>
+        <p className="font-bold">#webdeveloper</p>
+        <p className="font-bold">#Elon musk</p>
         <p className="font-bold">#HockeyWorldCup</p>
     </div>
   );
